5a7afa13927bbdcb403c2ac9b0587a565607f5a1
Harsh raj <harshraj9162192367@gmail.com>
2025-10-09 01:02:19 +0530

    debug API fetching error

--- 05currencyConvertor/src/App.jsx
+++ 05currencyConvertor/src/App.jsx
@@ -68,7 +68,7 @@ function App() {
                                 onCurrencyChange={(currency) =>
                                   setTo(currency)
                                 }
-                                selectCurrency={from}
+                                selectCurrency={to}
                                 amountDisable
                             />
                         </div>
--- 05currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 05currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -4,13 +4,12 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-)
+         fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
 .then((res) => res.json())
 .then((res) => setData(res[currency]))
-console.log(data);
+.catch((err) => console.error("error fetching currency data:", err));
     },[currency])
-    console.log(data);
+    // console.log(data);
     return data
 }
 
