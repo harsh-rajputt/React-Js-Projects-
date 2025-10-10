ab6f2acda159f81c5a33e4823f0c8c2296365aae
Harsh raj <harshraj9162192367@gmail.com>
2025-10-10 13:56:22 +0530

    create different route

--- 06reactRouter/src/main.jsx
+++ 06reactRouter/src/main.jsx
@@ -6,6 +6,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 const router = createBrowserRouter([
   {
@@ -18,6 +21,19 @@ const router = createBrowserRouter([
       },
       { path: "about",
         element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      },
+      {
+        path: "user/:userid",
+        element: <User/>
+      },
+      {
+        loader: githubInfoLoader,
+        path: "github",
+        element: <Github/>
       }
     ]
   }
