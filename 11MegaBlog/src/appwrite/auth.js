3c668ce5b5ae96d3407fc02959f71e542bdf9c40
Harsh raj <harshraj9162192367@gmail.com>
2025-10-17 11:10:49 +0530

    create authentication page

---
+++ 11MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,41 @@
+import conf from "../conf.js";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    Client = new Client();
+    account;
+
+    constructor() {
+        this.Client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appWriteProjectId);
+        this.account = new Account(this.Client);
+    }
+
+
+     async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+     }
+
+     async login({email, password}) {
+        try {
+           return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+     }    
+}
+
+const authService = new AuthService();
+
+export default authService
